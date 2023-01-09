ba4ed4840b031c09f2c7830fb8c17988b56598b4
S_Uboldi <solmaruboldi@gmail.com>
2023-01-08 23:27:32 -0300

    Función mostrar respuesta lista, falta mejorar encriptar/desencriptar

--- js/encriptar.js
+++ js/encriptar.js
@@ -1,10 +1,10 @@
-
 function encriptar() {
     let textoUsuario = document.querySelector("#textousuario").value;
     let textoEncriptado = textoUsuario.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/a/gi, "ai").replace(/o/gi, "ober").replace(/u/gi, "ufat");
     /* let textoEncriptado = textoUsuario.replace[(/a/g, "ai") && (/e/g, "enter") && (/i/g, "imes") && (/o/g, "ober") && (/u/g, "ufat")]; */
     document.querySelector("#textorespuesta").value = textoEncriptado;
     document.querySelector("#textousuario").value;
+    mostrarRespuesta();
 }
 
 let botonEncriptar = document.querySelector("#botonencriptar");
@@ -15,7 +15,22 @@ function desencriptar() {
     let textoDesencriptado = textoUsuario.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/ober/gi, "o").replace(/ufat/gi, "u");
     document.querySelector("#textorespuesta").value = textoDesencriptado;
     document.querySelector("#textousuario").value;
+    mostrarRespuesta();
 }
 
 let botonDesencriptar = document.querySelector("#botondesencriptar");
 botonDesencriptar.onclick = desencriptar;
+
+document.getElementById("respuestamostrada").style.display = "none";
+
+function mostrarRespuesta() {
+    if (botonEncriptar.onclick = encriptar) {
+        document.getElementById("respuestainicial").style.display = "none";
+        document.getElementById("respuestamostrada").style.display = "block";
+    } if (botonDesencriptar.onclick = desencriptar) {
+        document.getElementById("respuestainicial").style.display = "none";
+        document.getElementById("respuestamostrada").style.display = "block";
+    }
+}
+
+
